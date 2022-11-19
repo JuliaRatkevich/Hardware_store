c7e78f9cdae73a1beec139ece44d3418b09fc1c4
Julia <juliakuntsevich@gmail.com>
2022-11-19 18:40:46 +0100

    Initial Commit

---
+++ Hardware_store/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hardware_store
+//
+//  Created by Yuliya Ratkevcih on 19.11.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hardware_store/Hardware_storeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hardware_storeApp.swift
+//  Hardware_store
+//
+//  Created by Yuliya Ratkevcih on 19.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Hardware_storeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
